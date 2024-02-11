cdef9412e56a32ad7cc7a2d172836d05aa45e25a
blertton <blettonkr@gmail.com>
2024-02-11 02:01:18 +0100

    added gif

--- app/hero/hero.tsx
+++ app/hero/hero.tsx
@@ -32,18 +32,8 @@ const hero = () => {
           </p>
         </Reveal>
       </div>{" "}
-      <div className="items-center mx-auto bg-none relative w-full md:w-96 h-[30rem]  overflow-hidden">
-        {/* {" "}
-        <Image
-          src={deved}
-          alt="Developer Image"
-          layout="fill"
-          objectFit="cover"
-        /> */}
-        <Spline
-          className="w-96 h-80 bg-none"
-          scene="https://prod.spline.design/U-G5FVmGISpMK644/scene.splinecode"
-        />
+      <div className="relative bg-gradient-to-b from-gray-900 mx-auto rounded-full w-full md:w-80 h-80 mt-14 overflow-hidden">
+        <Image src={myGif} alt="my gif" className=" bg-none h-80 " />
       </div>
     </div>
   );
